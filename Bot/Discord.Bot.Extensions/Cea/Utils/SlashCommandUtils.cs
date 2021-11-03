8e145477e71b21029e6087982caa18f1cb1191dd
Bradley Holloway <bradley@holloway.fyi>
2021-11-02 19:52:49 -0500

    remove unused code.

--- Bot/Discord.Bot.Extensions/Cea/Utils/SlashCommandUtils.cs
+++ Bot/Discord.Bot.Extensions/Cea/Utils/SlashCommandUtils.cs
@@ -9,25 +9,6 @@ namespace Discord.Cea
 { 
     class SlashCommandUtils
     {
-        public static void AddCommonArguments(SlashCommandOptionBuilder builder, SlashCommandOptions slashCommandOptions)
-        {
-            if (slashCommandOptions.HasFlag(SlashCommandOptions.team))
-            {
-                builder.AddOption(
-                    name: SlashCommandOptions.team.ToString(),
-                    type: ApplicationCommandOptionType.String,
-                    description: "Filter command option to a specific team.");
-            }
-
-            if (slashCommandOptions.HasFlag(SlashCommandOptions.org))
-            {
-                builder.AddOption(
-                    name: SlashCommandOptions.org.ToString(),
-                    type: ApplicationCommandOptionType.String,
-                    description: "Filter command option to a specific organization (company).");
-            }
-        }
-
         public static IReadOnlyDictionary<SlashCommandOptions, string> OptionsToDictionary(IReadOnlyCollection<SocketSlashCommandDataOption> options)
         {
             Dictionary<SlashCommandOptions, string> optionsDictionary = new();
